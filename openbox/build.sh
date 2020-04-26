eeac2b13340b92fac43ab111e764fc47ee4fdd5b
Matthew Fosse <matt.cfosse@gmail.com>
2020-04-26 16:14:26 -0400

    saving

--- openbox/build.sh
+++ openbox/build.sh
@@ -17,9 +17,17 @@ fi
 cd $HOSTFILES
 
 # todo: check if necessary
-# npm i --production
-# npm run build:native
+npm i --production
 
+sudo apt install -y libx11-dev libxtst-dev libpng++-dev ffmpeg libasound2-dev
+
+# ensure same node version
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#nvm install 12.16.2
+#npm rebuild
 
 cd $CURRENT_DIR
 sudo docker build -t box:01 .
